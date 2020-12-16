8341dafe179516e46bf833b79144b6e2f1812847
Aaron Carnahan <aaron.b.carnahan@outlook.com>
2020-12-16 13:15:11 -0500

    first commit

---
+++ VTCT.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VTCT.WebMVC.Startup))]
+namespace VTCT.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
